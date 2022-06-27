9a268edd32b9259314e99d9cdf78287c58bc258e
Tim Ponarenko <user.name>
2022-06-27 17:34:17 +0400

    first push...

---
+++ src/components/Menu.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+export function MainMenu () {
+return(
+    <div className="Menu">
+<Link to="/" className="MenuItem">
+  {" "}
+  Tim{" "}
+</Link>
+<Link to="/project" className="MenuItem">
+  {" "}
+  პროექტები{" "}
+</Link>
+<Link to="/hobby" className="MenuItem">
+  {" "}
+  ჰობი{" "}
+</Link>
+</div>
+)
+};
+
+export function SubMenu () {
+  return(
+    <div className="Menu">
+    
+<Link to="/music" className="MenuItem">
+  {" "}
+  მუსიკა{" "}
+</Link>
+<Link to="/films" className="MenuItem">
+  {" "}
+  ფილმები{" "}
+</Link>
+    </div>
+  )
+}
+
+export function ProjectMenu () {
+  return(
+    <div className="Menu">
+   
+    </div>
+  )
+}
