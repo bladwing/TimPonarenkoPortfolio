5bc529ea9a82f46467e00859b6dcdd141a246cca
BLADWING <timoti.ponarenkO@gmail.com>
2022-07-24 21:18:12 +0400

    add information and experience json, create view for them

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,10 @@
 import React from "react";
-import Clock from "./widgets/Clock";
+
 
 export default function Footer() {
   return (
     <div className="footer">
-      <hr />
-რეალური დრო
-      <Clock />
 
-      <hr />
     </div>
   );
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,10 @@
+import React from "react";
 import { MainMenu } from "../Menu";
 import "./main.scss";
 import "./NameEffect.scss";
 import Tim from "../../assets/Tim.jpg";
-import Georgia from "../../assets/georgia.png";
+import stockData from "../../utils/Information.json";
+import Experience from "../../utils/experience.json";
 
 export default function Main() {
   return (
@@ -14,17 +16,51 @@ export default function Main() {
           <h1>Tim Ponarenko</h1>
         </div>
       </div>
-
       <MainMenu />
 
-      <div className="Content">
-        <div> Born : 1991</div>
-        <div>
-          {" "}
-          Country : Georgia{" "}
-          <img src={Georgia} className="geoFlag" alt="georgia_flag" />
-        </div>
-        <div>აქ იქნება ინფორმაცია...</div>
+      <div className="informationCard">
+        {stockData.map((id, key) => {
+          return (
+            <div key={key}>
+              <ul>
+                <span className="fuck">
+                  <b>{id.birthdayTitle}</b> {id.Birthday}
+                </span>
+                <span>
+                  <b>{id.contryTitle}</b> {id.Country}
+                </span>
+                <span>
+                  <b>{id.telephoneTitle}</b> {id.telephone}
+                </span>
+                <span>
+                  <b>{id.emailTitle}</b> {id.email}
+                </span>
+                <span>
+                  <b>{id.gitHubTitle}</b> {id.GitHub}
+                </span>
+              </ul>
+            </div>
+          );
+        })}
+      </div>
+      <h2 className="experienceTitle">გამოცდილება</h2>
+      <div className="experienceCard">
+        {Experience.map((exp) => {
+          return (
+            <div key={exp.id} data={exp}>
+              <div className="Card">
+                <img src={exp.imageLink} alt="logo" className="experienceLogo"/>
+                <h4>
+                  <li>{exp.title}</li>
+                </h4>
+
+                <div>თანამდეობა: {exp.jobtitle}</div>
+
+                <div>წლები: {exp.years}</div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
