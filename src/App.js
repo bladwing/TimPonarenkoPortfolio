1a8e7e42bb8249467c59f0c536e195c47a90aed8
BLADWING <timoti.ponarenkO@gmail.com>
2022-07-24 15:57:21 +0400

    create colors, fix for title and border

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Project, Experience, Hobby, Music, Films } from "./components/Pages";
-import Main from "./components/Main/Main"
+import Main from "./components/Main/Main";
 import Footer from "./components/Footer";
 import "./App.scss";
-import "./effects.scss";
+import "./scss/effects.scss";
 
 const App = () => {
   return (
     <div className="MainPage rainbow">
-    <Router>
-   
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/project" element={<Project />} />
-        <Route path="/experience" element={<Experience />} />
-        <Route path="/hobby" element={<Hobby />} />
-        <Route path="/music" element={<Music />} />
-        <Route path="/films" element={<Films />} />
-      </Routes>
-    </Router>
-    <Footer/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/hobby" element={<Hobby />} />
+          <Route path="/music" element={<Music />} />
+          <Route path="/films" element={<Films />} />
+        </Routes>
+      </Router>
+      <Footer />
     </div>
   );
-  
 };
 
 export default App;
