36a691abcdb1756dae75585d2ebc16360c0eb4a6
bladwing <timoti.ponarenko@gmail.com>
2022-08-03 17:52:55 +0400

    change translaiton for name

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React from "react";
 import Nav from "./components/Nav";
 import Social from "./components/social/Social";
 import LangSwitcher from "./components/langSwitcher/LangSwitcher";
+import { useTranslation } from "react-i18next";
 
 import "./App.scss";
 import "./scss/effects.scss";
 
 const App = () => {
+  const {t} = useTranslation();
   return (
     <div className="MainPage">
       <LangSwitcher/>
       <div className="wrapper Header">
-        <h1>Tim Ponarenko</h1>
+        <h1>{t("TitleName")}</h1>
       </div>
       <Nav />
       <Social />
