72c262ebec4a15cbb9e766042d2c4be49b5c2483
bladwing <timoti.ponarenko@gmail.com>
2022-12-11 17:10:52 +0400

    change experinece and style for experience

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -2,10 +2,13 @@ import ExpBase from "../../utils/experience.json";
 import ExperienceImg from "../../assets/experience.png";
 import { useTranslation } from "react-i18next";
 
+
 import "./exp.scss";
 
 export default function Experience() {
   const { t } = useTranslation();
+
+ 
   return (
     <div>
       <div className="PhotoAndHeader">
@@ -15,19 +18,24 @@ export default function Experience() {
 
       <div className="experienceCards">
         {ExpBase.map((exp) => {
+           
           return (
             <div key={exp.id} data={exp} className="cardContainer">
+              
               <div className="Card">
                 <img
                   src={exp.imageLink}
                   alt={exp.alt}
                   className="experienceLogo"
                 />
-                <h3>{exp.title}</h3>
+                <h3 style={{color: exp.link !== undefined ? "#21325E" : "black"}}>
+                  <a href={exp.link} target="_blank" rel="noopener noreferrer">{exp.title}</a>
+                </h3>
 
                 <div className="jobTitle"> {exp.jobtitle}</div>
 
                 <div>{exp.years}</div>
+
               </div>
             </div>
           );
