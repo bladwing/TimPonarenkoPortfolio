f904bab2dcbd077e83b6da8b2ccf8ca0dfaeb304
bladwing <timoti.ponarenko@gmail.com>
2023-01-07 11:28:24 +0400

    tree shaking in main page

--- src/components/main/MainPage.jsx
+++ src/components/main/MainPage.jsx
@@ -2,44 +2,16 @@ import React from "react";
 import { MainMenu } from "../Menu";
 import "./main.scss";
 import "./NameEffect.scss";
-// import Tim from "../../assets/Tim.jpg";
-// import infoData from "../../utils/Information.json";
 
 export default function Main() {
   return (
     <div>
       <div className="PhotoAndHeader">
-        {/* <img className="ProfilePhotoTim" src={Tim} alt="profilePhoto" /> */}
-
         <div className="wrapper Header">
           <h1>Tim Ponarenko</h1>
         </div>
       </div>
       <MainMenu />
-
-      {/* <div className="informationCard">
-        {infoData.map((id, key) => {
-          return (
-            <div key={key}>
-              <div>
-                <b>დაბადების დღე:</b> {id.birthday}
-              </div>
-              <div>
-                {" "}
-                <b>ტელეფონი:</b> {id.telephone}
-              </div>
-              <div>
-                {" "}
-                <b>ელ. ფოსტა:</b> {id.email}
-              </div>
-              <div>
-                <a href={id.github}>GitHub</a>
-              </div>
-            </div>
-          );
-        })}
-      </div> */}
-
     </div>
   );
 }
